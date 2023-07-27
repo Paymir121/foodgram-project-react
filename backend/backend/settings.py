d5a45294d25cc53816adbe2c4f6a1a260b0f6c5d
Nikita Romanov <RNN@pqegroup.com>
2023-07-27 15:50:34 +0300

    gitworkflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)3wvd287c1x4b#q*n1f5f%alc1bxjl78@(bpz%wt=ydf!q&rr0'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
