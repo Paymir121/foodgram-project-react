056036b91a216f387cefea8574758cc048d89367
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2023-07-31 15:14:48 +0300

    Update importcsv.py

--- backend/recipy/management/commands/importcsv.py
+++ backend/recipy/management/commands/importcsv.py
@@ -5,12 +5,8 @@ from django.conf import settings
 from django.core.management.base import BaseCommand 
 
 from recipy.models import Ingredient
-from users.models import User 
 
-TABLES = { 
 
-    Ingredient: 'ingredients.csv', 
-} 
 def get_reader(file_name): 
     csv_path = Path(settings.INITIAL_DATA_DIR, file_name) 
     csv_file = open(csv_path, 'r', encoding='utf-8') 
@@ -18,8 +14,10 @@ def get_reader(file_name):
     return reader 
 
 class Command(BaseCommand): 
-    def handle(self, *args, **options): 
-        for model, csv_file in TABLES.items(): 
-            csv_reader = get_reader(csv_file) 
-            model.objects.bulk_create(model(**data) for data in csv_reader) 
-        print('Import DONE')
+
+    def handle(self, *args, **options):
+        csv_reader = get_reader("ingredients.csv")
+        next(csv_reader)
+        Ingredient.objects.bulk_create([Ingredient(name=ingredeint['name'],
+                                                       measurement_unit=ingredeint['measurement_unit']) 
+                                                       for ingredeint in csv_reader]) 
