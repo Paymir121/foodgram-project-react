786ddb5b1c15f65e412b5d7cad0cfebd5b12e974
Paymir121 <nikox12@mail.ru>
2023-08-19 14:41:06 +0300

    precomit1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY_DJANGO", "key does not exist")
 
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
