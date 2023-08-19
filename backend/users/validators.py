d20506d01dac3747001ba52de1296884e755d7cf
Paymir121 <nikox12@mail.ru>
2023-08-19 13:36:02 +0300

    precomit1

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def validator_username(value):
-    if value.lower() == 'me':
-        raise ValidationError('me слово запрещенное, в любом регистре.')
+    if value.lower() == "me":
+        raise ValidationError("me слово запрещенное, в любом регистре. ")
     return value
